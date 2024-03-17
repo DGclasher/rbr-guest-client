bcbb9c0b5eb585dcc8d7536b018c28fb0fc133a9
DGclasher <73327721+DGclasher@users.noreply.github.com>
2024-03-17 16:08:25 +0530

    fix team card

--- src/data/Team.js
+++ src/data/Team.js
@@ -2,25 +2,25 @@
 const teamData = [
     {
         id: 1,
-        name: 'RavindraBabu  Rabula',
-        role: 'CEO, Raudra Technologies',
+        name: 'RavindraBabu  Ravula',
+        role: 'CEO',
         image: '/rbr.webp',
         desc: 'Drives innovation and guides strategic decisions.'
     },
     {
         id: 2,
-        name: 'Santosh Naidu ',
-        role: 'CTO, Raudra Technologies',
-        image: '/santosh.webp',
-        desc: 'Leads technological initiatives and fosters innovation.'
-    },
-    {
-        id: 3,
         name: 'Mudeen Shaik',
-        role: 'CTO, Raudra Technologies',
+        role: 'CTO',
         image: '/mudeen.webp',
         desc: 'Drives product development via innovative technological strategies.'
     },
+    {
+        id: 3,
+        name: 'Santosh Naidu ',
+        role: 'Technical Lead',
+        image: '/santosh.webp',
+        desc: 'Leads technological initiatives and fosters innovation.'
+    }
 ]
 
 export default teamData;
